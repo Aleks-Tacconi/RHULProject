c0b0a4689bc846c0fff9fc6fab523ceb4c975f63
Christopher White <cwhite.jan.06@gmail.com>
2025-04-01 16:25:31 +0100

    a

--- entities/portal.py
+++ entities/portal.py
@@ -1,2 +1,7 @@
-class Portal:
-    pass
+from entities import Background
+from utils import Vector
+
+
+class Portal(Background):
+    def __init__(self, pos: Vector, img: str, size_x: int, size_y: int, scale_factor: int) -> None:
+        super().__init__(pos=pos, img=img, size_x= size_x, size_y=size_y, scale_factor=scale_factor)
